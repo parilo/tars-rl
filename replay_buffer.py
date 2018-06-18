894240c806d7bc03bb3a8e369d582cb6544d1834
AlexGrinch <grinchuk.alexey@gmail.com>
2018-06-18 14:04:22 +0000

    pep8 code

--- replay_buffer.py
+++ replay_buffer.py
@@ -6,8 +6,9 @@ from threading import Lock
 ############################################# Huge server buffer ############################################
 #############################################################################################################
 
+
 class ServerBuffer:
-    
+
     def __init__(self, capacity, history_len=1, num_of_parts_in_obs=1):
         self.size = capacity
         self.hist_len = history_len
@@ -16,15 +17,15 @@ class ServerBuffer:
         self.observations = [None for part_id in range(self.num_parts)]
         self.actions = None
         self.rewards = None
-        self.dones = None   
+        self.dones = None
         self._store_lock = Lock()
         self.transition = namedtuple('Transition', ('s', 'a', 'r', 's_', 'done'))
-        
+
     def push_episode(self, episode):
         """episode = [observations, actions, rewards, dones]
         """
-        
-        with self._store_lock:  
+
+        with self._store_lock:
             observations, actions, rewards, dones = episode
             self.obs_shapes = [observations[part_id][0].shape for part_id in range(self.num_parts)]
             self.act_shape = actions[0].shape
@@ -49,62 +50,63 @@ class ServerBuffer:
             self.dones[indices] = np.array(dones)
 
             self.pointer = (self.pointer + episode_len) % self.size
-        
+
     def get_transition(self, idx):
-        
+
         state, next_state = [], []
         for part_id in range(self.num_parts):
             s = np.zeros((self.hist_len, ) + self.obs_shapes[part_id], dtype=np.float32)
             s[-1] = self.observations[part_id][idx]
             for i in range(self.hist_len-1):
-                if (idx-i-1<0 or self.dones[idx-i-1]): break
+                if (idx-i-1 < 0 or self.dones[idx-i-1]): break
                 s[-2-i] = self.observations[part_id][idx-i-1]
-                
+
             s_ = np.zeros_like(s)
             s_[:-1] = s[1:]
             if not self.dones[idx]:
-                s_[-1] = self.observations[part_id][(idx+1)%self.size]
-                
+                s_[-1] = self.observations[part_id][(idx + 1) % self.size]
+
             state.append(s)
             next_state.append(s_)
-            
+
         action = self.actions[idx]
         reward = self.rewards[idx]
         done = self.dones[idx]
-        
+
         return state, action, reward, next_state, done
-    
+
     def get_random_indices(self, num_indices):
         indices = np.arange(self.num_in_buffer)
         np.random.shuffle(indices)
         return indices[:num_indices]
-    
+
     def get_batch(self, batch_size):
         indices = self.get_random_indices(batch_size)
         transitions = []
         for idx in indices:
             transitions.append(self.get_transition(idx))
-            
+
         states = []
         for part_id in range(self.num_parts):
             state = np.array([transitions[i][0][part_id] for i in range(batch_size)])
-            states.append(state)        
+            states.append(state)
         actions = np.array([transitions[i][1] for i in range(batch_size)])
-        rewards = np.array([transitions[i][2] for i in range(batch_size)])        
+        rewards = np.array([transitions[i][2] for i in range(batch_size)])
         next_states = []
         for part_id in range(self.num_parts):
             next_state = np.array([transitions[i][3][part_id] for i in range(batch_size)])
             next_states.append(next_state)
         dones = np.array([transitions[i][4] for i in range(batch_size)])
-        batch = self.transition(states, actions, rewards, next_states, dones)     
+        batch = self.transition(states, actions, rewards, next_states, dones)
         return batch
 
 #############################################################################################################
 ###################################### Small local buffer of each agent #####################################
 #############################################################################################################
-    
+
+
 class AgentBuffer:
-    
+
     def __init__(self, capacity, history_len=1, num_of_parts_in_obs=1):
         self.size = capacity
         self.hist_len = history_len
@@ -112,22 +114,23 @@ class AgentBuffer:
         self.observations = [None for part_id in range(self.num_parts)]
         self.actions = None
         self.rewards = None
-        self.dones = None   
-        
+        self.dones = None
+
     def push_init_observation(self, obs):
         for part_id in range(self.num_parts):
             self.observations[part_id] = np.empty((self.size, ) + obs[part_id].shape, dtype=np.float32)
             self.observations[part_id][:self.hist_len-1] = np.zeros((self.hist_len-1,) + obs[part_id].shape)
             self.observations[part_id][self.hist_len-1] = obs[part_id]
         self.pointer = self.hist_len-1
-        
+
     def get_current_state(self):
         state = []
         for part_id in range(self.num_parts):
-            s = self.observations[part_id][self.pointer-self.hist_len+1:self.pointer+1]
+            indices = range(self.pointer-self.hist_len+1, self.pointer)
+            s = self.observations[part_id][indices]
             state.append(s)
         return state
-    
+
     def push_transition(self, transition):
         """ transition = [next_obs, action, reward, done]
         """
@@ -136,14 +139,14 @@ class AgentBuffer:
             self.actions = np.empty((self.size, ) + action.shape, dtype=np.float32)
             self.rewards = np.empty((self.size, ), dtype=np.float32)
             self.dones = np.empty((self.size, ), dtype=np.bool)
-            
+
         for part_id in range(self.num_parts):
             self.observations[part_id][self.pointer+1] = next_obs[part_id]
         self.actions[self.pointer] = action
         self.rewards[self.pointer] = reward
         self.dones[self.pointer] = done
         self.pointer += 1
-        
+
     def get_complete_episode(self):
         indices = np.arange(self.hist_len-1, self.pointer)
         observations = []
--- rl_server/server/rl_client.py
+++ rl_server/server/rl_client.py
@@ -2,15 +2,16 @@ import threading
 from .tcp_client_server import TCPClient
 from .serialization import serialize, deserialize
 
+
 def obs_to_string(observations):
     """ Convert observations (or states) to strings for transmission to server.
-    
+
     Parameters
     ----------
     observations: list of np.arrays [obs_1, ..., obs_n]
         which corresponds to the original observations (or states)
         each np.array obs_i has shape (batch_size) + obs_shape_i
-       
+
     Returns
     -------
     str_obs: list of strings [str_1, ..., str_n]
@@ -21,8 +22,9 @@ def obs_to_string(observations):
         str_obs.append(obs.reshape(-1).tostring())
     return str_obs
 
+
 def episode_to_req(episode, method='store_exp_batch'):
-    """ Create compact serialized representation of the episode 
+    """ Create compact serialized representation of the episode
         to pass it as a request.
     """
     observations, actions, rewards, dones = episode
@@ -30,15 +32,19 @@ def episode_to_req(episode, method='store_exp_batch'):
     str_act = actions.tolist()
     str_rew = rewards.tolist()
     str_don = dones.tolist()
-    req = serialize({'method':method, 'observations':str_obs, 'actions':str_act,
-                     'rewards':str_rew, 'dones':str_don})
+    req = serialize({'method': method,
+                     'observations': str_obs,
+                     'actions': str_act,
+                     'rewards': str_rew,
+                     'dones': str_don})
     return req
+
 
 class RLClient:
 
     def __init__(self, ip_address='127.0.0.1', port=8777, network_timeout=120):
         """ Class for RL Client which interacts with RL Server.
-    
+
         Parameters
         ----------
         ip_address: str
@@ -51,7 +57,7 @@ class RLClient:
         self._tcp_client = TCPClient(ip_address, port, network_timeout)
         self._tcp_client.connect()
         self._tcp_lock = threading.Lock()
-        
+
     def act(self, state):
         """
             state is list of state parts
@@ -69,11 +75,12 @@ class RLClient:
             differently in the NN
         """
         str_states = obs_to_string(states)
-        req = serialize({'method': 'act_batch', 'states': str_states})
+        req = serialize({'method': 'act_batch',
+                         'states': str_states})
         with self._tcp_lock:
             data = self._tcp_client.write_and_read_with_retries(req)
             return deserialize(data)
- 
+
     def store_exp(self, transition):
         self.store_exp_batch(transition)
 
