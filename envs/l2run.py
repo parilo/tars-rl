230eced1658bcb1b0821d081c8c5f2292377ad36
Anton Pechenko <forpost78@gmail.com>
2018-07-08 13:31:51 +0300

    fixed l2run. now begin to converge

--- envs/l2run.py
+++ envs/l2run.py
@@ -7,6 +7,7 @@ import numpy as np
 class ExtRunEnv(L2RunEnv):
 
     def __init__(self, frame_skip=1, visualize=False):
+        self.vis = visualize
         self.env = L2RunEnv(visualize=visualize)
         self.frame_skip = frame_skip
         self.observation_shapes = [(41,)]
@@ -22,7 +23,7 @@ class ExtRunEnv(L2RunEnv):
     def step(self, action):
         reward = 0
         for i in range(self.frame_skip):
-            observation, r, done, info = self.env.step(action) 
+            observation, r, done, info = self.env.step(action)
             reward += r
             if done: break
         observation = self.preprocess_obs(observation)
@@ -32,34 +33,42 @@ class ExtRunEnv(L2RunEnv):
 
     def preprocess_obs(self, obs):
 
-        # 0-2   -- position of the pelvis (rotation, x, y)
-        # 3-5   -- velocity of the pelvis (rotation, x, y)
-        # 6-11  -- rotation of each ankle, knee and hip (6 values)
-        # 12-17 -- angular velocity of each ankle, knee and hip (6 values)
-        # 18-19 -- position of the center of mass (x, y)
-        # 20-21 -- velocity of the center of mass (x, y)
-        # 22-35 -- positions (x, y) of head, pelvis, torso, left and 
-        #          right toes, left and right talus (14 values)
-        # 36-37 -- strength of left and right psoas: 1 for difficulty < 2,
-        #          otherwise a random normal variable with mean 1 and standard
-        #          deviation 0.1 fixed for the entire simulation
-        # 38-40 -- next obstacle: x distance from the pelvis,
-        #          y position of the center relative to the the ground, radius
+        # res += state_desc["joint_pos"]["ground_pelvis"] 3 rot, x, y
+        # res += state_desc["joint_vel"]["ground_pelvis"] 3 rot, x, y
+        # 6
+        #
+        # bedro, koleno, lodizhka
+        # for joint in ["hip_l","hip_r","knee_l","knee_r","ankle_l","ankle_r",]:
+        #     res += state_desc["joint_pos"][joint]
+        #     res += state_desc["joint_vel"][joint]
+        # 2 * 6 = 12 rot, rot_v
+        # 18
+        #
+        # for body_part in ["head", "pelvis", "torso", "toes_l", "toes_r", "talus_l", "talus_r"]:
+        #     res += state_desc["body_pos"][body_part][0:2]
+        # 2 * 7 = 14 x, y
+        # 32
+        #
+        # res = res + state_desc["misc"]["mass_center_pos"] + state_desc["misc"]["mass_center_vel"]
+        # 4
+        # 36
+        #
+        # res += [0]*5
+        # 41
 
         obs = np.array(obs)
         x = obs[1]
         y = obs[2]
         a = obs[0]
-        obs[[1, 24, 25]] = 0
-        obs[6:12] -= a
-        obs[[18, 22, 26, 28, 30, 32, 34]] -= x
-        obs[[19, 23, 27, 29, 31, 33, 35]] -= y
-        obs[38] /= 100.0
+        obs[[0, 20, 21]] = 0
+        obs[[6, 8, 10, 12, 14, 16]] -= a
+        obs[[18, 22, 24, 26, 28, 30, 32]] -= x
+        obs[[19, 23, 25, 27, 29, 31, 33]] -= y
         return obs.copy()
 
     def get_total_reward(self):
         return self.total_reward
-    
+
     def get_random_action(self, resample=True):
         if resample:
             self.init_action = np.round(np.random.uniform(0., 0.7, size=self.action_size))
